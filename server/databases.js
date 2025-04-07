e38ce4f353f2ae39fe1e9db6426de15510c58a6a
Selvie <selviesec@gmail.com>
2025-04-07 03:22:03 +0200

    fix

--- server/databases.js
+++ server/databases.js
@@ -1,42 +1,43 @@
-// const mysql = require('mysql2');
-// require('dotenv').config();
+const mysql = require('mysql2');
+require('dotenv').config();
 
-// const db = mysql.createConnection({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME
-// });
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+});
 
-// db.connect(err => {
-//     if (err) {
-//         console.error('Database connection failed:', err.stack);
-//         return;
-//     }
-//     console.log('Connected to MySQL database.');
-// });
+db.connect((err) => {
+    if (err) {
+        console.error("Database connection failed: " + err.message);
+    } else {
+        console.log("Connected to MySQL Database!");
+    }
+});
 
-// module.exports = db;
 
-// const mysql = require('mysql')
-// const app = require('express')
+// userQueries.js
+const db = require('./db');
 
-// const db = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: 'Mydatabase123456',
-// })
+function findUserByEmail(email, cb) {
+  const sql = 'SELECT * FROM users WHERE email = ?';
+  db.query(sql, [email], (err, results) => {
+    if (err) return cb(err);
+    // Return the first result if found, otherwise null
+    return cb(null, results.length ? results[0] : null);
+  });
+}
 
-// app.get('/createdb', (req, res) => {
-//     let sql = 'CREATE DATABASE nodemysql'
-//     db.query(sql, (err, result) => {
-//         if(err) throw err;
-//         console.log(result)
-//         res.send('database created')
-//     })
-// })
+function findUserById(id, cb) {
+  const sql = 'SELECT * FROM users WHERE id = ?';
+  db.query(sql, [id], (err, results) => {
+    if (err) return cb(err);
+    return cb(null, results.length ? results[0] : null);
+  });
+}
 
-// db.connect(function(err) {
-//   if (err) throw err;
-//   console.log("Connected!");
-// });
+module.exports = { findUserByEmail, findUserById };
+
+
+module.exports = db;
